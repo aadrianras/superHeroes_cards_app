07ba449dccaefd409e381940d97de12951897f79
Adrian <aadrianras@gmail.com>
2021-01-19 23:03:50 -0400

    Imagenes corregidas con proces.env.PUBLIC_URL

--- src/components/heroes/HeroCard.js
+++ src/components/heroes/HeroCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance, characters, handlePopUp }) => {
-
+    const imgHero = `/assets/heroes/${id}.jpg`;
     return (
         <div className="heroes_card" onClick={() => handlePopUp({ id, show: true })}>
             <div className="heroes_card_content">
                 <div>
-                    <img src={`/assets/heroes/${id}.jpg`} className="hero_card_img" alt={superhero} />
+                    <img src={imgHero} className="hero_card_img" alt={superhero} />
                 </div>
                 <div className="heroes_card_text">
                     <p><span>Álter ego:</span> {alter_ego}</p>
