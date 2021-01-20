1d60832d6a7605733d2314081724b0b3ee563a41
Adrian <aadrianras@gmail.com>
2021-01-19 23:11:53 -0400

    nueva prueba

--- src/components/heroes/HeroCard.js
+++ src/components/heroes/HeroCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance, characters, handlePopUp }) => {
-    const imgHero = `/assets/heroes/${id}.jpg`;
+    const imgHero = `${process.env.PUBLIC_URL}/assets/heroes/${id}.jpg`;
     return (
         <div className="heroes_card" onClick={() => handlePopUp({ id, show: true })}>
             <div className="heroes_card_content">
